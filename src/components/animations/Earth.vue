def63ffb15a80ee162c907e3f13980fcb6e58900
Daymer Perdomo <daimer003@gmail.com>
2025-10-16 16:40:37 -0500

    feat: about

---
+++ src/components/animations/Earth.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative w-[600px] xl:w-[713px] h-[600px] xl:h-[713px]">
+    <img
+      src="@/assets/images/earth.png"
+      alt="planeta animado"
+      class="absolute  w-full h-full object-contain animate-rotate-slow "
+    />
+  </div>
+</template>
+
+<style scoped>
+@keyframes rotate-slow {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.animate-rotate-slow {
+  animation: rotate-slow 350s linear infinite;
+}
+</style>
